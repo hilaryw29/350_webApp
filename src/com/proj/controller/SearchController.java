d8fa772191602e599579827e4826543bb9fff89c
jujulian <duzhuolunno1@gail.com>
2020-11-22 18:24:59 -0500

    Add SearchController.java

---
+++ src/com/proj/controller/SearchController.java
@@ -0,0 +1,56 @@
+package com.proj.controller;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.proj.dao.ListingDao;
+import com.proj.model.Listing;
+
+public class SearchController extends HttpServlet {
+    /**
+     * This class only handles the SEARCH feature of the web app.
+     *
+     * These are variables that lead to the appropriate JSP pages.
+     *
+     * SEARCH_USER leads to the search results page.
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private static String SEARCH_USER = "/listingResult.jsp";
+    private ListingDao dao;
+
+    /**
+     * Constructor for this class.
+     */
+    public SearchController() {
+        super();
+        dao = new ListingDao();
+    }
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        /**
+         * This method handles the retrieval of the search keyword entered by
+         * the user.
+         */
+        String keyword = request.getParameter("keyword");
+
+        RequestDispatcher view = request.getRequestDispatcher(SEARCH_USER);
+        request.setAttribute("keyword", keyword);
+        request.setAttribute("listings", dao.getLisitngByKeyword(keyword));
+        /**
+         * Redirect to the search results page after the list of students
+         * matching the keywords has been retrieved.
+         */
+
+        view.forward(request, response);
+    }
+}
