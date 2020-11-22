cdead5683020e33226d83eceb6e6d62ff8053363
duzhuolun <55144854+duzhuolun@users.noreply.github.com>
2020-11-22 17:27:57 -0500

    added delete listing, get listing by category, get listing by keyword

--- src/com/proj/dao/ListingDao.java
+++ src/com/proj/dao/ListingDao.java
@@ -5,7 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.mie.model.Student;
 import com.proj.model.*;
 import com.proj.controller.*;
 import com.mie.util.DbUtil;
@@ -45,5 +48,81 @@ public class ListingDao {
 		}
 		return 0;
 	}
+
+	public int deleteListing(int listingID){
+		/**
+		 * This method deletes a listing from the database.
+		 */
+		try {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("delete from listing where listingID=?");
+			preparedStatement.setInt(1, listingID);
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public List<Listing> getLisitngByCategory (String category){
+		/**
+		 * This method retrieves a list of listing that matches the category
+		 * selected by the user.
+		 */
+		List<Listing> listings = new ArrayList<Listing>();
+
+		try {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("select * from listing where category=?");
+
+			preparedStatement.setString(1, category);
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				Listing listing = new Listing();
+				listing.setListingId(rs.getInt("listingID"));
+				listing.setTitle(rs.getString("title"));
+				listing.setDescription(rs.getString("description"));
+				listing.setPrice(rs.getDouble("price"));
+				listing.setCategory(rs.getString("category"));
+				listing.setImagePath(rs.getString("image"));
+				listing.setUserId(rs.getInt("userID"));
+				listings.add(listing);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return listings;
+	}
+
+	public List<Listing> getLisitngByKeyword (String keyword){
+		/**
+		 * This method retrieves a list of listing that matches the keyword
+		 * entered by the user.
+		 */
+		List<Listing> listings = new ArrayList<Listing>();
+
+		try {
+			PreparedStatement preparedStatement = connection
+					.prepareStatement("select * from listing where title LIKE ?");
+			preparedStatement.setString(1, keyword);
+			ResultSet rs = preparedStatement.executeQuery();
+			while (rs.next()) {
+				Listing listing = new Listing();
+				listing.setListingId(rs.getInt("listingID"));
+				listing.setTitle(rs.getString("title"));
+				listing.setDescription(rs.getString("description"));
+				listing.setPrice(rs.getDouble("price"));
+				listing.setCategory(rs.getString("category"));
+				listing.setImagePath(rs.getString("image"));
+				listing.setUserId(rs.getInt("userID"));
+				listings.add(listing);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return listings;
+	}
+
 
 }
